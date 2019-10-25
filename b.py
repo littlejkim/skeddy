485084cef050728d9bc4ff5606142a5c97a1315c
lpaqkosw <jaehoonlee1994@gmail.com>
2019-10-26 02:52:02 +0900

    b

---
+++ b.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import datetime as dt
+import html5lib
+
+def pastinfo(code, page):
+    
+    url = ('https://finance.naver.com/item/sise_day.nhn?code='+code+'&page='+str(page))
+    data = pd.read_html(url)
+    data = data[0]
+    data.columns = ['날짜','당일종가','전일종가','시가', '고가', '저가', '거래량']
+    price_data = data.dropna(axis=0, how='any')
+    price_data = price_data.drop(price_data.index[0])
+    price_data = price_data.reset_index(drop=True)
+    price_data['날짜'] = pd.to_datetime(price_data['날짜'], format='%Y/%m/%d')
+    return price_data
+    
+
+pd = pastinfo("215600", 1)
+
+print(pd)
+
